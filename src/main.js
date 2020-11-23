2d9f5e9dac42846d0cf4d314eb85565fbaa1dd07
josefa <sefaherrera391993@google.com>
2020-11-23 13:22:42 -0400

    cambios en la línea 10 de webpack.config.js, agregado style en main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  //console.log("Hello Rigo from the console!");
+  document.querySelector("body").style.background = "#0E7E12";
 };
