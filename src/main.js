6333241ea54bc3adf0bda40debaa9dd8d22005de
josefa <sefaherrera391993@google.com>
2020-11-23 22:45:33 -0400

    agregado button

--- src/main.js
+++ src/main.js
@@ -2,29 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
+var generateRandomNumber = () => {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let randomNumber = Math.floor(Math.random() * numbers.length);
+  return numbers[randomNumber];
+};
+var generateRandomSuit = () => {
+  let suits = ["♠", "♣", "♦", "♥"];
+  let randomSuit = Math.floor(Math.random() * suits.length);
+  return suits[randomSuit];
+};
+//var colorCard = () => {
+//  "♥" || "♦" ? (card.style.color = "red") : (card.style.color = "black");
+//};
+
 window.onload = function() {
+  var button = document.createElement("button");
+  button.type = "button";
+  button.innerText = "Nueva Carta";
+  document.body.appendChild(button);
+  document.button.addEvenListener(click, function{
+     document.card.innerHTML = suit;
+  });
   //console.log("Hello Rigo from the console!");
   document.querySelector("body").style.background = "#0E7E12";
-  var card = (document.querySelector(".card").style.background = "white");
-  var top = (document.querySelector(".suit-top").style.justifyContent =
-    "flex-start");
-  var middle = (document.querySelector(".suit-middle").style.justifyContent =
-    "center");
-  var bottom = (document.querySelector(".suit-bottom").style.justifyContent =
-    "flex-end");
-  
-  var generateRandomNumber = middle => {
-    let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
-    let randomNumber = Math.floor(Math.random() * numbers.length);
-    return numbers[randomNumber];
-  };
-  var generateRandomSuit = (top, bottom) => {
-    let suits = ["♠", "♣", "♦", "♥"];
-    let randomSuit = Math.floor(Math.random() * suits.length);
-    return suits[randomSuit];
-  };
-  var colorCard = card => {
-    "♥" && "♦" ? (card.style.color = "red") : (card.style.color = "black");
-  };
-  
+  var card = document.querySelector(".card");
+  card.style.background = "white";
+  var top = document.querySelector(".suit-top");
+  var middle = document.querySelector(".suit-middle");
+  var bottom = document.querySelector(".suit-bottom");
+
+  middle.textContent = generateRandomNumber();
+  var suit = generateRandomSuit();
+  top.textContent = suit;
+  bottom.textContent = suit;
+
+  if (suit === "♥" || suit === "♦") {
+    card.style.color = "red";
+  } else {
+    card.style.color = "black";
+  }
 };
